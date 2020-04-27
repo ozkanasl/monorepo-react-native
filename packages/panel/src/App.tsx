b1ae47abe0a0d5d9448a6e7d3e3a92d8e8584427
Tmob015 <tmob015@Tmob015s-MacBook-Pro.local>
2020-04-27 14:24:59 +0300

    edit

---
+++ packages/panel/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SharedHome from 'shared/src/screens/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <SharedHome />
+    </div>
+  );
+}
+
+export default App;
